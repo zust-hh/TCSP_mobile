d0a433c19f3d8a6e52bd1eebe5c844e1ab7e8ad3
zust_hh <819473021@qq.com>
2017-12-09 22:56:15 +0800

    begin travelmain
    begin travelmain

--- TCSP_App/pages/LaunchImage.js
+++ TCSP_App/pages/LaunchImage.js
@@ -4,36 +4,162 @@ import {
   StyleSheet,
   Text,
   View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Dimensions,
   ScrollView,
-  Button
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import Home from './Home';
-import Regist from './Regist';
-import Login from './Login';
-import List from './TestList';
-import Admin from './Admin';
-class LaunchImage extends Component {
-  static navigationOptions = {
-    title: 'Launch',    //设置navigator的title
+var width = Dimensions.get('window').width;
+export default class LauchImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scoreName: ['太糟了', '失望哦', '很一般', '还成吧', '极好的'],
+      onePoint: {
+        name: '小和山风景区',
+        longitude: 120.3,
+        latitude: 30.2,
+        feel: '山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方山清水秀，是个旅游的好地方',
+        commentNum: 3,
+        commentList: [{
+          userName: '',
+          content: '好地方',
+          time: '',
+          score: 3,
+        }, {
+          userName: '',
+          content: '好地方',
+          time: '',
+          score: 4,
+        }, {
+          userName: '',
+          content: '好地方',
+          time: '',
+          score: 5,
+        }
+        ]
+      },
+    }
   }
   render() {
-    const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Button
-          onPress={() => navigate('Admin')}
-          title="Chat with Lucy"
-        />
-      </View>
+      <ScrollView style={styles.container}>
+        <Image source={require('../public/images/image2.jpg')} style={styles.headimage} />
+        <View style={styles.feel}>
+          <View style={styles.feelhead}>
+            <Image source={require('../public/images/feelicon.png')} style={styles.feelheadicon} />
+            <Text style={{ fontWeight: 'bold' }}>游记分享</Text>
+
+          </View>
+          <View style={styles.feelcontent}>
+            <Text>       {this.state.onePoint.feel}</Text>
+          </View>
+        </View>
+        <View style={styles.pointmap}>
+          <Image source={{ uri: 'http://restapi.amap.com/v3/staticmap?key=a12fe0a773225a0edbb395bce289a441&scale=2&zoom=14&markers=mid,,A:' + this.state.onePoint.longitude + ',' + this.state.onePoint.latitude + '&location=' + this.state.onePoint.longitude + ',' + this.state.onePoint.latitude + '&size=' + width + '*120' }} style={{ width: width, height: 120 }} />
+        </View>
+        <TouchableOpacity style={styles.addcomment} activeOpacity={0.9} onPress={() => alert(1)}>
+          <Image source={require('../public/images/nullstar.png')} style={{ width: 240, height: 60 }} />
+          <Text style={{ color: 'rgb(168,168,168)' }}>点击添加评论</Text>
+        </TouchableOpacity>
+        <View style={styles.commentmain}>
+          <View style={styles.commenthead}>
+            <Image source={require('../public/images/commenticon.png')} style={styles.commenticon} />
+            <Text style={{ fontWeight: 'bold' }}>4.32评分  3评论</Text>
+          </View>
+          <View style={styles.commentlist}>
+            <View style={styles.onecomment}>
+              <Image source={require('../public/images/boy.png')} style={{width:45,height:45}}/>
+              <View style={styles.onecommentright}>
+                <View style={styles.onecommentrighthead}>
+                  <Text>Zust_lxz</Text>
+                  <View>
+                    <Text>很一般</Text>
+                  </View>
+                </View>
+                <View>
+                  <Text>这里真是个好地方！！！</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
-export default SimpleApp = StackNavigator({
-  LaunchImage: { screen: LaunchImage },
-  Home: { screen: Home },
-  Regist: { screen: Regist },
-  Login: { screen: Login },
-  List: { screen: List },
-  Admin: { screen: Admin },
-});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(238,238,238)',
+  },
+  headimage: {
+    width: width,
+    height: 160,
+  },
+  feel: {
+    marginTop: 8,
+    backgroundColor: '#FFFFFF',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+  },
+  feelhead: {
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+
+  },
+  feelheadicon: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+  commenticon: {
+    width: 19,
+    height: 17,
+    marginRight: 10,
+  },
+  feelcontent: {
+
+  },
+  pointmap: {
+    height: 120,
+    marginTop: 8,
+    backgroundColor: '#FFFFFF',
+  },
+  addcomment: {
+    marginTop: 8,
+    width: width,
+    height: 90,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center'
+  },
+  commentmain: {
+    marginTop: 8,
+    backgroundColor: '#FFFFFF',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+  },
+  commenthead: {
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+  },
+  commentlist: {
+    backgroundColor: '#FFFFFF',
+    marginTop: 8,
+
+  },
+  onecomment: {
+    flexDirection: 'row',
+  },
+  onecommentrighthead: {
+    flexDirection: 'row',
+  }
+})
+
+module.exports = LauchImage;
---
+++ TCSP_App/pages/TravelMain.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ListView,
+} from 'react-native';
+import forge from 'node-forge';
+import ButtonComponent from 'react-native-button-component';
+var { height, width } = Dimensions.get('window');
+export default class TravelMain extends Component {
+  constructor(props) {
+    super(props);
+    this.renderItem = this.renderItem.bind(this);
+    this.renderHeaderItem = this.renderHeaderItem.bind(this);
+    this.renderFooterItem = this.renderFooterItem.bind(this);
+    this.state = {
+      pointList: [{
+        pointNum: 1,
+        pointName: '上海的奇妙之旅',
+        pointCity: '上海市',
+      },
+      {
+        pointNum: 2,
+        pointName: '上海的奇妙之旅',
+        pointCity: '上海市',
+      },
+      {
+        pointNum: 3,
+        pointName: '上海的奇妙之旅',
+        pointCity: '上海市',
+      }],
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      pointEn: []
+    }
+  }
+  static navigationOptions = {
+    header: false,
+  }
+  //进行渲染数据
+  renderContent(dataSource) {
+    return (
+      <ListView
+        initialListSize={1}
+        dataSource={dataSource}
+        renderRow={this.renderItem}
+        style={{ flex: 1 }}
+        onEndReachedThreshold={10}
+        enableEmptySections={true}
+        renderHeader={this.renderHeaderItem}
+        renderFooter={this.renderFooterItem}
+      />
+    );
+  }
+  //渲染每一项的数据
+  renderItem(data) {
+    if (data.pointNum > 1 && data.pointNum < this.state.pointList.length + 1) {
+      return (this.renderCenterItem(data));
+    }
+  }
+  renderHeaderItem() {
+    return (
+      <View style={{ flexDirection: 'row', height: 75 }}>
+        <View>
+          <Image source={require('../public/images/beginpoint.png')}
+            style={{ width: 30, height: 30, marginLeft: 10, marginTop: 22 }} />
+          <Image source={require('../public/images/ic_order_shu.png')} style={{ height: 20, marginLeft: 25, flex: 1 }} />
+        </View>
+        <View >
+          <View style={{ height: 5 }} />
+          <Image source={require('../public/images/ic_order_status_item_bg.png')}
+            style={{ height: 65, marginLeft: 10, width: (width - 60) }}>
+            {this.renderCenterContent(this.state.pointList[0])}
+          </Image>
+          <View style={{ height: 5 }} />
+        </View>
+      </View>
+    );
+  }
+  renderCenterItem(data) {
+    return (
+      <View style={{ flexDirection: 'row', height: 75 }}>
+        <View>
+          <Image source={require('../public/images/ic_order_shu.png')} style={{ height: 20, marginLeft: 25, flex: 1 }} />
+          <Image source={require('../public/images/centerpoint.png')} style={{ width: 30, height: 30, marginLeft: 10 }} />
+          <Image source={require('../public/images/ic_order_shu.png')} style={{ height: 20, marginLeft: 25, flex: 1 }} />
+        </View>
+        <View >
+          <View style={{ height: 5 }} />
+          <Image source={require('../public/images/ic_order_status_item_bg.png')}
+            style={{ height: 65, marginLeft: 10, width: (width - 60) }}>
+            {this.renderCenterContent(data)}
+          </Image>
+          <View style={{ height: 5 }} />
+        </View>
+      </View>
+    );
+  }
+  renderFooterItem() {
+    if (this.state.pointList.length > 1) {
+      return (
+        <View style={{ flexDirection: 'row', height: 75 }}>
+          <View>
+            <Image source={require('../public/images/ic_order_shu.png')} style={{ height: 20, marginLeft: 25 }} />
+            <Image source={require('../public/images/endpoint.png')} style={{ width: 30, height: 30, marginLeft: 10 }} />
+          </View>
+          <View >
+            <View style={{ height: 5 }} />
+            <Image source={require('../public/images/ic_order_status_item_bg.png')}
+              style={{ height: 65, marginLeft: 10, width: (width - 60) }}>
+              {this.renderCenterContent(this.state.pointList[this.state.pointList.length - 1])}
+            </Image>
+            <View style={{ height: 5 }} />
+          </View>
+        </View>
+      );
+    }
+    else return null;
+  }
+  renderCenterContent(data) {
+    let md = forge.md.md5.create();
+    let md5 = '20171204000102168' + data.pointName + '1435660288' + 'e7lsCMhXMlCsuflPXkrO';
+    md5 = encodeURI(md5);
+    md.update(md5);
+    let sign = md.digest().toHex();
+    // alert(sign);
+    let trauri = "http://api.fanyi.baidu.com/api/trans/vip/translate?q=" + encodeURI(data.pointName) + "from=zh&to=en&appid=20171204000102168&salt=1435660288&sign=" + sign;
+    fetch(trauri)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          console.error('服务器繁忙，请稍后再试；\r\nCode:' + response.status)
+        }
+      })
+      .then((data1) => {
+        let pointEn = JSON.stringify(data1);
+        // alert(pointEn);
+        // this.setState({ pointEn }, () => {
+        //   alert(this.state.pointEn);
+        // })
+      })
+      .catch((err) => {
+        console.error(err)
+      });
+    return (
+      <View style={{ marginLeft: 15, marginTop: 10 }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ color: 'black', fontSize: 14, backgroundColor: '#00000000' }}>{data.pointName}</Text>
+          <View style={{ flex: 1, alignItems: 'flex-end', marginRight: 10 }}><Text style={{ color: '#777', fontSize: 12, backgroundColor: '#00000000' }}>{data.pointCity}</Text></View>
+        </View>
+        <Text style={{ color: '#777', fontSize: 12, marginTop: 10, backgroundColor: '#00000000' }}>this.</Text>
+      </View>
+    );
+  }
+  // componentWillMount() {
+  //   var pointList = JSON.parse(JSON.stringify(this.props.navigation)).state.params.pointList;
+  //   this.setState({ pointList });
+  // }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.travelhead}>
+          <Text style={{ fontSize: 30, color: '#fff', marginTop: 30, marginLeft: 25 }}>第一次旅行</Text>
+
+        </View>
+        <View style={{ flex: 1, backgroundColor: '#f5f5f5', paddingTop: 20 }}>
+          {this.renderContent(this.state.dataSource.cloneWithRows(
+            this.state.pointList.slice(1, this.state.pointList.length - 1) === undefined ? [] : this.state.pointList.slice(1, this.state.pointList.length - 1)))}
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  travelhead: {
+    width: width,
+    height: 160,
+    backgroundColor: 'rgba(24,150,136,0.8)',
+  },
+  goback: {
+    width: 100,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    zIndex: 99999
+  },
+  complete: {
+    width: 100,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    zIndex: 99999
+  }
+});
